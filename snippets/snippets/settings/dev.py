c008c54c89cdc33ead0e94be3a57d7d0f19dd119
abdullah mohamed <abdommg4@fci.helwan.edu.eg>
2018-04-08 22:16:31 +0200

    Starting the project and configuring settings Starting the Project and configuring settings

---
+++ snippets/snippets/settings/dev.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'snippet_db',
+        'USER': 'postgres',
+        'PASSWORD': 'ammg1998',
+        'HOST': 'localhost',
+        'PORT': '5432',
+}
+
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
